523240320bc2f159d50c3b8baf060dfdfb481fdd
Mahizabin <123229709+Mahizabin@users.noreply.github.com>
2023-06-11 08:52:52 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    //Odd number printing in the given range
+    for(x=1;x<22;x+=2){
+        cout<<x<<endl;
+    }
+    cout<<endl;
+    //Even number printing in the given range
+    for(x=0;x<21;x+=2){
+        cout<<x<<endl;
+    }
+    return 0;
+}
+
